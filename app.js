a1dbb19b702cc333389e97d46e6e5152ebb42692
Smooty Ziah <ehydlebeerg2@gmail.com>
2022-04-12 21:00:51 -0400

    new push

---
+++ app.js
@@ -0,0 +1,41 @@
+
+var a = 10;
+var b = 20;
+
+
+
+function addSum (param1,param2) {
+
+    var addSum = (param1 + param2) / 2;
+    console.log(addSum);
+  return addSum;
+    
+}
+addSum(10,20);
+
+function addSum (param1,param2) {
+
+    var addSum = (param1 - param2) / 2;
+    console.log(addSum);
+  return addSum
+    
+}
+addSum(10,20);
+
+function addSum (param1,param2) {
+
+    var addSum = (param1 * param2) / 2 ;
+    console.log(addSum);
+  return addSum
+    
+}
+addSum(10,20);
+
+function addSum (param1,param2) {
+
+    var addSum = (param1 / param2) / 2;
+    console.log(addSum);
+  return addSum
+    
+}
+addSum(10,20);
